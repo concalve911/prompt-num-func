d867d83a523cf53dce710fedc8ac8afa1dab563a
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-30 15:54:18 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function promptNumber() {
+  let lastInput;
+  for (let i = 0; i < 10; i++) {
+    lastInput = prompt("Please enter a number more then 100");
+    if (!lastInput?.trim() || isNaN(lastInput)) {
+      alert("Your enter is cancelled");
+      return;
+    }
+    lastInput = Number(lastInput);
+    if (!isNaN(lastInput) && lastInput > 100) {
+      alert("Your entered the number more then 100");
+      return;
+    }
+  }
+  console.log(`Maximum attempts reached. Last input was:, ${lastInput}`);
+}
+
+promptNumber();
